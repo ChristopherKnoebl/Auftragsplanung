639d3f6b46496857b6895e75dcda608fc040ae0d
ChristopherKnoebl <christopher-knoebl@web.de>
2024-06-05 18:59:19 +0200

    Fenster zur Auftragserstellung eingefügt

--- main.py
+++ main.py
@@ -4,7 +4,37 @@ from tkinter import ttk
 
 main = tkinter.Tk()
 
-buFenster = ttk.Button(main, text="Auftrag erstellen")
+def oeffneAuftragsErstellung():
+    FensterAuftragserstellung = tkinter.Toplevel(main)
+    FensterAuftragserstellung.title("Auftrag erstellen")
+
+    lbProdukt = tkinter.Label(FensterAuftragserstellung, text="Produkt-ID:")
+    lbProdukt.grid(row=0, column=0, sticky="w", padx=5, pady=5)
+    etProduktID = tkinter.Entry(FensterAuftragserstellung)
+    etProduktID.grid(row=0, column=1, padx=5, pady=5)
+
+    lbMenge = tkinter.Label(FensterAuftragserstellung, text="Menge:")
+    lbMenge.grid(row=1, column=0, sticky="w", padx=5, pady=5)
+    etMenge = tkinter.Entry(FensterAuftragserstellung)
+    etMenge.grid(row=1, column=1, padx=5, pady=5)
+
+    lbAnfangdatum = tkinter.Label(FensterAuftragserstellung, text="Auftragsbeginn:")
+    lbAnfangdatum.grid(row=2, column=0, sticky="w", padx=5, pady=5)
+    etAnfangdatum = tkinter.Entry(FensterAuftragserstellung)
+    etAnfangdatum.grid(row=2, column=1, padx=5, pady=5)
+
+    lbEnddatum = tkinter.Label(FensterAuftragserstellung, text="Auftragsende")
+    lbEnddatum.grid(row=3, column=0, sticky="w", padx=5, pady=5)
+    etEnddatum = tkinter.Entry(FensterAuftragserstellung)
+    etEnddatum.grid(row=3, column=1, padx=5, pady=5)
+
+    lbAusgabe = tkinter.Label(FensterAuftragserstellung, text="Auftrag-Erstellen")
+    lbAusgabe.grid(row=4, column=0, padx=5, pady=5)
+    
+    buAuftragErstellen = tkinter.Button(FensterAuftragserstellung, text ="Erstelle Auftrag", command= erstelleAuftrag)
+    buAuftragErstellen.grid(row=4, column=1, padx=5, pady=5)
+
+buFenster = ttk.Button(main, text="Auftrag erstellen", command=oeffneAuftragsErstellung)
 buFenster.grid(row=0, column=0, padx=5, pady=5)
 
 buFenster = ttk.Button(main, text="Tabelle abfragen")
