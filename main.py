806990c6d0ad5f104f6e875c91a2e18a28036335
ChristopherKnoebl <christopher-knoebl@web.de>
2024-06-05 19:08:27 +0200

    Fenster zur Tabellenabfrage eingefügt

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import tkinter
 import sqlite3
 from tkinter import ttk
 
+#öffne das Hauptfenster
 main = tkinter.Tk()
 
+# erstell
 def oeffneAuftragsErstellung():
+    """
+    Mit dieser Funktion wird ein neues Fenster erstellt, in dem Aufträge eingegebenw werden können
+    """
     FensterAuftragserstellung = tkinter.Toplevel(main)
     FensterAuftragserstellung.title("Auftrag erstellen")
 
@@ -31,13 +36,32 @@ def oeffneAuftragsErstellung():
     lbAusgabe = tkinter.Label(FensterAuftragserstellung, text="Auftrag-Erstellen")
     lbAusgabe.grid(row=4, column=0, padx=5, pady=5)
     
-    buAuftragErstellen = tkinter.Button(FensterAuftragserstellung, text ="Erstelle Auftrag", command= erstelleAuftrag)
+    buAuftragErstellen = tkinter.Button(FensterAuftragserstellung, text ="Erstelle Auftrag")
     buAuftragErstellen.grid(row=4, column=1, padx=5, pady=5)
 
 buFenster = ttk.Button(main, text="Auftrag erstellen", command=oeffneAuftragsErstellung)
 buFenster.grid(row=0, column=0, padx=5, pady=5)
 
-buFenster = ttk.Button(main, text="Tabelle abfragen")
+def oeffneTabellenbfrage():
+    Tabellenabfrage = tkinter.Toplevel(main)
+    Tabellenabfrage.title("Tabelle abfragen")
+
+    lbEingabe = tkinter.Label(Tabellenabfrage, text="Ihre Eingabe:")
+    lbEingabe.grid(row=0, column=0, sticky="w", padx=5, pady=5)
+
+    etEingabe = tkinter.Entry(Tabellenabfrage)
+    etEingabe.grid(row=1, column=0, padx=5, pady=5)
+
+    buAbfragen = tkinter.Button(Tabellenabfrage, text="Abfragen", width=10)
+    buAbfragen.grid(row=2, column=0, sticky="w", padx=5, pady=5)
+
+    lbAusgabe = tkinter.Label(Tabellenabfrage, text="")
+    lbAusgabe.grid(row=3, column=0, sticky="w", padx=5, pady=5)
+
+    lbTabelle = tkinter.Label(Tabellenabfrage, text="")
+    lbTabelle.grid(row=0, column=1, padx=5, pady=5)
+
+buFenster = ttk.Button(main, text="Tabelle abfragen", command=oeffneTabellenbfrage)
 buFenster.grid(row=0, column=1, padx=5, pady=5)
 
 tkinter.mainloop()
