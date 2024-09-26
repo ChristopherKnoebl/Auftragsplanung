c072cf2148e09fa41a5a6ac145c767ede33da418
ChristopherKnoebl <christopher-knoebl@web.de>
2024-09-26 22:24:16 +0200

    Elemente des Controllers ausgelagert

--- main.py
+++ main.py
@@ -1,108 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
-import pyodbc
-
-# Klassen aus dem Model
-# Klasse Fertigungsaufträge 
-class Fertigungsauftraege:
-    def __init__(self, auftragsnummer, produktname, menge, auftragsbeginn, auftragsende):
-        self.auftragsnummer = auftragsnummer
-        self.produktname = produktname
-        self.menge = menge
-        self.auftragsbeginn = auftragsbeginn
-        self.auftragsende = auftragsende
-        self.timestamp = None
-
-    def kalkuliere_materialbedarf(self):
-        print("Materialbedarf wird kalkuliert...")
-
-    def erstelle_stueckliste(self):
-        print("Stückliste wird erstellt...")
-
-    def deckt_lagerbestand_materialbedarf(self):
-        print("Prüfe, ob der Lagerbestand den Materialbedarf deckt...")
-
-# Klasse Lager
-class Lager():
-    SERVER = 'Chris-PC'
-    DATABASE = 'Beispiel-Firma'
-    USERNAME = '<username>'
-    PASSWORD = '<password>'
-
-    def __init__(self, name) -> None:
-        self.name = name
-
-    def lager_abfrage(self, ID=0):
-        connectionString = f"""
-        DRIVER={{ODBC Driver 18 for SQL Server}};
-        SERVER={Lager.SERVER};
-        DATABASE={Lager.DATABASE};
-        Trusted_Connection=yes;
-        TrustServerCertificate=yes;
-"""
-        try:
-            # Datenbankanbindung
-            conn = pyodbc.connect(connectionString)
-            cursor = conn.cursor()
-            print("Verbindung hergestellt")
-            if ID == 0:
-                SQL = f"""
-                SELECT * FROM [Beispiel-Firma].[dbo].[{self.name}];
-                """
-                print(f"{ID=}")
-                cursor.execute(SQL)
-            else:
-                SQL = f"""
-                SELECT * FROM [Beispiel-Firma].[dbo].[{self.name}]
-                WHERE ID = 1;
-                """
-                cursor.execute(SQL)
-
-            ergebnisse = []
-            records = cursor.fetchall()
-            for r in records:
-                ergebnisse.append(r)
-            print(ergebnisse)
-            
-        except pyodbc.Error as e:
-            print(f"Fehler bei der Datenbankverbindung: {e}")
-        finally:
-            cursor.close()
-            conn.close()
-        return f"{ergebnisse=}"
-model1 = Lager("Materialien")
-print(model1.lager_abfrage())
-
-# Controller-Klasse
-class LagerController :
-    def __init__(self) -> None:
-        pass
-
-class AuftragsController:
-    def __init__(self):
-        self.auftraege = []  # Liste von Fertigungsaufträgen
-
-    def erstelle_auftrag(self, auftragsnummer, produktname, menge, auftragsbeginn, auftragsende):
-        auftrag = Fertigungsauftraege(auftragsnummer, produktname, menge, auftragsbeginn, auftragsende)
-        self.auftraege.append(auftrag)
-        print(f"Auftrag {auftragsnummer} erstellt: {produktname}, Menge: {menge}")
-        return auftrag
-
-    def frage_auftrag_ab(self, auftragsnummer):
-        for auftrag in self.auftraege:
-            if auftrag.auftragsnummer == auftragsnummer:
-                print(f"Auftrag {auftragsnummer} gefunden: {auftrag.produktname}")
-                return auftrag
-        print(f"Auftrag {auftragsnummer} nicht gefunden.")
-        return None
-
-    def frage_bestand_ab(self):
-        print("Bestand wird abgefragt...")
-        return {"Produkt A": 100, "Produkt B": 50}
-
-    def disponiere_bestand(self):
-        print("Bestand wird disponiert...")
-        return True
+from controller import Controller
 
 # Klassen aus der GUI
 # Hauptfenster-Klasse wird zur Elternklasse
@@ -295,6 +192,6 @@ class Auftragserstellung(Hauptfenster):
 # Main-Loop
 if __name__ == "__main__":
     root = tk.Tk()
-    controller = AuftragsController()
+    controller = Controller()
     app = Hauptfenster(root, controller)
     root.mainloop()
