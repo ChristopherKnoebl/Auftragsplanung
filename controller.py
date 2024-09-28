8dcb5013f6b5aedeeb94302f178ff8e418949c8a
ChristopherKnoebl <christopher-knoebl@web.de>
2024-09-28 20:43:06 +0200

    Lagerabfrage implementiert

--- controller.py
+++ controller.py
@@ -2,33 +2,34 @@
 from tkinter import messagebox
 from model import Lager
 
+
 class Controller:
     def __init__(self):
-        pass
+        self.lager = Lager()
+        
 
     # Methoden zur Lagerabfrage
     # Von der view erhalten
-    # müssen noch an das Modell übergeben werden
     def lager_abfragen(self,lagername, ID=0):
-
-        messagebox.showinfo("Der Controller hat die Anfrage:", f"{lagername=}, {ID=} erhalten")
-    # muss noch durchgeprüft werden, Anschluss an Datenbank notwendig
-    # return muss noch an das modell weiter gegeben werden
+        ergebnis = self.lager.lager_abfrage(lagername, ID)
+        return ergebnis
 
     # Methoden zur Lagerdisposition
+    # muss noch an das Modell übergeben werden
     def zubuchen_bestand(self, ID, menge):
         messagebox.showinfo("Der Controller hat die Anfrage:", f"{ID=}, {menge=} erhalten")
 
+        # muss noch an das Modell übergeben werden
     def ausbuchen_bestand(self, ID, menge):
         messagebox.showinfo("Der Controller hat die Anfrage:", f"{ID=}, {menge=} erhalten")
         
-    
+    # muss noch an das Modell übergeben werden
     # Methoden für die Auftragsplanung
     def auftrag_abfragen(self, ID):
         messagebox.showinfo("Der Controller hat die Anfrage:", f"{ID=} erhalten")
 
     # Methode für die Auftragserstellung
-
+    # muss noch an das Modell übergeben werden
     def auftrag_erstellen(self, matNr, menge, start, ende):
         messagebox.showinfo("Der Controller hat die Anfrage:", f"{matNr=}, {menge=}, {start=}, {ende=} erhalten")
         
